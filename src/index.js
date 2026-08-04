6aefe60743c29267d3a8668aac5b41da9a9f9593
metacomunicacaoarquivo-oss <metacomunicacaoarquivo@gmail.com>
2026-08-04 10:24:20 -0300

    Conectar rota dos criativos

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { metaBusinessOverview } from "./routes/metaBusiness.js";
 import { metaAdsCampaigns } from "./routes/metaAds.js";
 import { metaAdSets } from "./routes/metaAdSets.js";
 import { metaAdsItems } from "./routes/metaAdsItems.js";
+import { metaCreatives } from "./routes/metaCreatives.js";
 
 export default {
   async fetch(request, env) {
@@ -22,22 +23,31 @@ export default {
     if (url.pathname === "/instagram/dashboard") {
       return instagramDashboard(env);
     }
-if (url.pathname === "/facebook/overview") {
-  return facebookOverview(env);
-}
-if (url.pathname === "/meta-business/overview") {
-  return metaBusinessOverview(env);
-}
-if (url.pathname === "/meta-ads/campaigns") {
-  return metaAdsCampaigns(env, request);
-}
-if (url.pathname === "/meta-ads/adsets") {
-  return metaAdSets(env, request);
-}
-if (url.pathname === "/meta-ads/ads") {
-  return metaAdsItems(env, request);
-}
-    
+
+    if (url.pathname === "/facebook/overview") {
+      return facebookOverview(env);
+    }
+
+    if (url.pathname === "/meta-business/overview") {
+      return metaBusinessOverview(env);
+    }
+
+    if (url.pathname === "/meta-ads/campaigns") {
+      return metaAdsCampaigns(env, request);
+    }
+
+    if (url.pathname === "/meta-ads/adsets") {
+      return metaAdSets(env, request);
+    }
+
+    if (url.pathname === "/meta-ads/ads") {
+      return metaAdsItems(env, request);
+    }
+
+    if (url.pathname === "/meta-ads/creatives") {
+      return metaCreatives(env, request);
+    }
+
     return Response.json(
       {
         success: false,
